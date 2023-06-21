99a4c2c57966ffb1f8c13cc56d71e7df58743496
Arthur Lucas <123699745+ArthurSous4@users.noreply.github.com>
2023-06-21 16:09:31 -0300

    Add files via upload

--- Aulas_Java_CDD/src/Fundamentos/exercicio06.java
+++ Aulas_Java_CDD/src/Fundamentos/exercicio06.java
@@ -3,7 +3,9 @@ package Fundamentos;
 public class exercicio06 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a = 3;
+		
+		System.out.println(a % 2 == 0 ? ++a : a++);
 
 	}
 
